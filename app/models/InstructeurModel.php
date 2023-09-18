5b4f720cb729fca9f7b6d925345266b7e4f496d9
Kenan-88 <113502900+Kenan-88@users.noreply.github.com>
2023-09-18 19:42:23 +0200

    opdracht inleveren

---
+++ app/models/InstructeurModel.php
@@ -0,0 +1,132 @@
+<?php
+
+class InstructeurModel
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getInstructeurs()
+    {
+        $sql = "SELECT Id
+                      ,Voornaam
+                      ,Tussenvoegsel
+                      ,Achternaam
+                      ,Mobiel
+                      ,DatumInDienst
+                      ,AantalSterren
+                FROM  Instructeur
+                ORDER BY AantalSterren DESC";
+
+        $this->db->query($sql);
+        return $this->db->resultSet();
+    }
+
+    public function getToegewezenVoertuigen($Id)
+    {
+        $sql = "SELECT       VOER.Id
+                            ,VOER.Type
+                            ,VOER.Kenteken
+                            ,VOER.Bouwjaar
+                            ,VOER.Brandstof
+                            ,VOER.TypeVoertuigId
+                            ,TYVO.TypeVoertuig
+                            ,TYVO.RijbewijsCategorie
+
+                FROM        Voertuig    AS  VOER
+                
+                INNER JOIN  TypeVoertuig AS TYVO
+
+                ON          TYVO.Id = VOER.TypeVoertuigId
+                
+                INNER JOIN  VoertuigInstructeur AS VOIN
+                
+                ON          VOIN.VoertuigId = VOER.Id
+                
+                WHERE       VOIN.InstructeurId = $Id
+                
+                ORDER BY    TYVO.RijbewijsCategorie DESC";
+
+        $this->db->query($sql);
+        return $this->db->resultSet();
+    }
+    public function getToegewezenVoertuig($instructeurId, $voertuigId)
+    {
+        $sql = "SELECT       VOER.Id
+                            ,VOER.Type
+                            ,VOER.Kenteken
+                            ,VOER.Bouwjaar
+                            ,VOER.Brandstof
+                            ,VOER.TypeVoertuigId
+                            ,TYVO.TypeVoertuig
+                            ,TYVO.RijbewijsCategorie
+
+                FROM        Voertuig    AS  VOER
+                
+                INNER JOIN  TypeVoertuig AS TYVO
+
+                ON          TYVO.Id = VOER.TypeVoertuigId
+                
+                INNER JOIN  VoertuigInstructeur AS VOIN
+                
+                ON          VOIN.VoertuigId = VOER.Id
+                
+                WHERE       VOIN.InstructeurId = $instructeurId AND VOER.Id = $voertuigId
+                
+                ORDER BY    TYVO.RijbewijsCategorie DESC";
+
+        $this->db->query($sql);
+        return $this->db->resultSet();
+    }
+
+    public function getInstructeurById($Id)
+    {
+        $sql = "SELECT Voornaam
+                      ,Tussenvoegsel
+                      ,Achternaam
+                      ,DatumInDienst
+                      ,AantalSterren
+                FROM  Instructeur
+                WHERE Id = $Id";
+
+        $this->db->query($sql);
+
+        return $this->db->single();
+    }
+    function updateVoertuig_instructeur($voertuigId)
+    {
+        $sql = "UPDATE VoertuigInstructeur SET InstructeurId =:instructeur WHERE VoertuigId =  $voertuigId";
+        $this->db->query($sql);
+        $this->db->bind(':instructeur', $_POST['instructeur']);
+
+
+        return $this->db->resultSet();
+    }
+    function updateVoertuig($voertuigId)
+    {
+        $sql = "UPDATE Voertuig SET Type = :type, Brandstof = :brandstof, Kenteken = :kenteken, TypeVoertuigId = :typevoertuig WHERE 
+                Id = $voertuigId ";
+        $this->db->query($sql);
+        $this->db->bind(':type', $_POST['type']);
+        $this->db->bind(':brandstof', $_POST['brandstof']);
+        $this->db->bind(':typevoertuig', $_POST['typevoertuig']);
+        $this->db->bind(':kenteken', $_POST['kenteken']);
+
+
+
+        return $this->db->resultSet();
+    }
+    function getTypeVoertuig()
+    {
+        $sql = "SELECT Id
+                      ,TypeVoertuig
+                      ,RijbewijsCategorie
+                FROM  TypeVoertuig
+                ORDER BY RijbewijsCategorie DESC";
+        $this->db->query($sql);
+        return $this->db->resultSet();
+    }
+}
